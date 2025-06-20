ef93d55fe5f2656a240f6b4ae35d125ce8e6344d
Guilherme Carvalho <99284514+guilhermevon@users.noreply.github.com>
2025-06-20 08:18:33 -0300

    alterações para github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/conversorMoedas/', // 👈 isso aqui é essencial
   plugins: [react()],
 })
